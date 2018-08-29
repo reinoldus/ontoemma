f24c980715498b500cd3a26878c76eb3954b6b18
Stefan Hesse <kontakt@stefan-hesse.net>
2018-08-29 15:50:36 +0200

    source miniconda

--- run_emma.sh
+++ run_emma.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+source ~/miniconda3/bin/activate
+
 if [ "$1" = "cuda" ]; then
     python train_ontoemma.py -c config/ontoemma_nn_sample_experiments_cuda.json -m /mnt -p nn
 else
